42e0bdf73e58330751a011b44158e4b01dbe15e3
itallo.vieira.glo <itallo.sa.vieira@gmail.com>
2024-01-18 11:11:29 -0300

    feat: add routes to index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
+app.use(routes);
 
 app.listen(5555, () => {
   console.log('✅ Server sarted at http://localhost:5555');
-})
+});
